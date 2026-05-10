07b31e31bb7db54b569792bf55d942ef5dd8e10f
LincolnRicardo <lincoln.ricardo.profissional@gmail.com>
2026-05-10 01:24:54 -0300

    fix: reorganização das migrations e models

--- gerenciamento/times/models.py
+++ gerenciamento/times/models.py
@@ -6,6 +6,10 @@ from django.db import models
 class Time(models.Model):
     nome = models.CharField(max_length=100)
     cidade = models.CharField(max_length=100)
+    estadio= models.CharField(max_length=100)
+    tecnico= models.CharField(max_length=100)
+    data_fundacao= models.DateTimeField()
 
     def __str__(self):
         return self.nome
+    
